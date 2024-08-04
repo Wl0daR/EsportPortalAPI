9a76264ce90ec9379ff9c663b19f33c92f98d64f
Adrian Włodarczyk <adrian.wlodarczyk01@gmail.com>
2024-08-04 15:27:21 +0200

    2nd Commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Server.Kestrel.Https;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,13 +11,26 @@ builder.Services.AddDbContext<EsportContext>(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Skonfiguruj Kestrel do u¿ywania certyfikatu SSL na porcie 7123
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(7123, listenOptions =>
+    {
+        listenOptions.UseHttps("C:\\Certyfikaty\\OpenSSL-Win64\\selfsigned.pfx");
+    });
+});
+
 var app = builder.Build();
 
 // Skonfiguruj potok HTTP.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "EsportPortalAPI V1");
+        c.RoutePrefix = string.Empty;  // Sprawi, ¿e swagger bêdzie dostêpny pod adresem root, np. https://localhost:7123/
+    });
 }
 
 app.UseHttpsRedirection();
